8c356c6a1b56f18155f304d3bbb2d538b9e219b7
neo <egomi120@gmail.com>
2017-08-27 19:24:26 +0200

    Wprowadzanie dwóch liczb.

--- src/DodawaniezKlawiatury.java
+++ src/DodawaniezKlawiatury.java
@@ -4,11 +4,18 @@ public class DodawaniezKlawiatury {
 
     public static void main(String args[]){
 
-        System.out.print("Podaj liczbę ");
+        System.out.print("Podaj liczbę nr1: ");
 
-        Scanner sc = new Scanner(System.in);
-        int liczba = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        int liczba1 = sc1.nextInt();
 
-        System.out.print(liczba);
+        System.out.print("Podaj liczbę nr2: ");
+
+        Scanner sc2 = new Scanner(System.in);
+        int liczba2 = sc2.nextInt();
+
+        System.out.println("Liczba n1: " + liczba1);
+        System.out.println("Liczba n2: " + liczba2);
+
     }
 }
