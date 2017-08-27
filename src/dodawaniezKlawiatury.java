22e1a686237ed6c6a8fd3b06565abe57ad092d76
neo <egomi120@gmail.com>
2017-08-27 18:43:55 +0200

    initial commit

---
+++ src/dodawaniezKlawiatury.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class dodawaniezKlawiatury{
+
+    public static void main(String args[]){
+
+        System.out.print("Podaj liczbę ");
+
+        Scanner sc = new Scanner(System.in);
+        int liczba = sc.nextInt();
+
+        System.out.print(liczba);
+    }
+}
